5dee749832ba64aeea2f5779cbcb8167d566efae
Nitesh Sharma <niteshsharma9199@gmail.com>
2022-08-20 21:42:04 +0530

    First commit

---
+++ Customer.cs
@@ -0,0 +1,33 @@
+﻿namespace Getset
+{
+    public partial class Program
+    {
+        public class Customer
+        {
+            private int Id;
+            private string Name;
+            public void SetInfo(int id, string name)
+            {
+                if (id != 0)
+                {
+                    this.Id = id;
+
+                }
+                if (!String.IsNullOrEmpty(name))
+                {
+                    this.Name = name;
+                }
+            }
+            public int GetId()
+            {
+                return Id;
+            }
+            public string GetName()
+            {
+                return Name;
+            }
+                
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Getset
+{
+    public partial class Program
+    {
+        public static void Main()
+        {
+            //object for class
+            var nitesh = new Customer();
+
+            //accessing the method insted of field (encapsulation)
+            nitesh.SetInfo(1, "nitesh");
+            Console.WriteLine("Id: {0} Name: {1}",nitesh.GetId(), nitesh.GetName());
+            
+            
+
+        }
+    }
+}
